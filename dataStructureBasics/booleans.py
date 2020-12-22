9a8877a8c76c54fd28f8ffc8ea55d99d65fb7962
Noel Mendoza <nmendozavd@gmail.com>
2020-12-22 15:03:43 -0800

    completed booleans intro

---
+++ dataStructureBasics/booleans.py
@@ -0,0 +1,15 @@
+# Booleans: true or false statements
+
+# capitalize for booleans
+True
+False
+
+greater = 1 > 2
+equal = 1 == 1
+
+# create variable with nothing
+b = None
+
+
+print(greater)
+print(equal)
